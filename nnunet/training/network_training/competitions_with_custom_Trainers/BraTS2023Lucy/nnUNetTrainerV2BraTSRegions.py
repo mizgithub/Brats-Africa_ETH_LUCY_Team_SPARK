1633bc9103211a8b61543c567a3afacfd8115575
mizgithub <mizanu143@gmail.com>
2023-10-03 10:58:04 +0300

    oct 3 10:58 am

--- nnunet/training/network_training/competitions_with_custom_Trainers/BraTS2023Lucy/nnUNetTrainerV2BraTSRegions.py
+++ nnunet/training/network_training/competitions_with_custom_Trainers/BraTS2023Lucy/nnUNetTrainerV2BraTSRegions.py
@@ -87,7 +87,9 @@ class nnUNetTrainerV2BraTSRegions_BN_lucy_small_nnunet(nnUNetTrainerV2):
         if torch.cuda.is_available():
             self.network.cuda()
         self.network.inference_apply_nonlin = torch.nn.Softmax(1)
-        
+    def setup_DA_params(self):
+        nnUNetTrainerV2.setup_DA_params(self)
+        self.data_aug_params['unsharp_masking_lucy'] = True
 
 
 class nnUNetTrainerV2BraTSRegions(nnUNetTrainerV2):
