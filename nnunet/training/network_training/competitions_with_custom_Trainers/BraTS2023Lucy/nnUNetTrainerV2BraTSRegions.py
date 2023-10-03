7aa42150109ce8d1a75d1c6087570ee6f586e720
mizgithub <mizanu143@gmail.com>
2023-10-03 10:26:48 +0300

    oct 10:26am

--- nnunet/training/network_training/competitions_with_custom_Trainers/BraTS2023Lucy/nnUNetTrainerV2BraTSRegions.py
+++ nnunet/training/network_training/competitions_with_custom_Trainers/BraTS2023Lucy/nnUNetTrainerV2BraTSRegions.py
@@ -62,6 +62,32 @@ class nnUNetTrainerV2BraTSRegions_BN(nnUNetTrainerV2):
             self.network.cuda()
         self.network.inference_apply_nonlin = torch.nn.Softmax(1)
 
+class nnUNetTrainerV2BraTSRegions_BN_lucy_small_nnunet(nnUNetTrainerV2):
+    def initialize_network(self):
+        if self.threeD:
+            conv_op = nn.Conv3d
+            dropout_op = nn.Dropout3d
+            norm_op = nn.BatchNorm3d
+
+        else:
+            conv_op = nn.Conv2d
+            dropout_op = nn.Dropout2d
+            norm_op = nn.BatchNorm2d
+
+        norm_op_kwargs = {'eps': 1e-5, 'affine': True}
+        dropout_op_kwargs = {'p': 0, 'inplace': True}
+        net_nonlin = nn.LeakyReLU
+        net_nonlin_kwargs = {'negative_slope': 1e-2, 'inplace': True}
+        self.network = Generic_UNet(self.num_input_channels, self.base_num_features, self.num_classes,
+                                    len(self.net_num_pool_op_kernel_sizes),
+                                    self.conv_per_stage, 2, conv_op, norm_op, norm_op_kwargs, dropout_op,
+                                    dropout_op_kwargs,
+                                    net_nonlin, net_nonlin_kwargs, True, False, lambda x: x, InitWeights_He(1e-2),
+                                    self.net_num_pool_op_kernel_sizes, self.net_conv_kernel_sizes, False, True, True,max_num_features=128)
+        if torch.cuda.is_available():
+            self.network.cuda()
+        self.network.inference_apply_nonlin = torch.nn.Softmax(1)
+
 
 class nnUNetTrainerV2BraTSRegions(nnUNetTrainerV2):
     def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
